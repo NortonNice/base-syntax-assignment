c7fdd4bbeab1fe8a759ba3043dc02a4fb5510efb
Norton Nice <nortonnice@gmail.com>
2019-03-06 12:08:44 -0800

    Updates

---
+++ precache-manifest.8129e8d3620826cce18b9e291a1561ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74180da249c02997f146",
+    "url": "/base-syntax-assignment/static/js/runtime~main.74180da2.js"
+  },
+  {
+    "revision": "7e77553921c2c448de49",
+    "url": "/base-syntax-assignment/static/js/main.7e775539.chunk.js"
+  },
+  {
+    "revision": "b8d28f15da4a44937a23",
+    "url": "/base-syntax-assignment/static/js/2.b8d28f15.chunk.js"
+  },
+  {
+    "revision": "7e77553921c2c448de49",
+    "url": "/base-syntax-assignment/static/css/main.45d2a99d.chunk.css"
+  },
+  {
+    "revision": "abf24c2247da9d03898e6ffae5b2cfad",
+    "url": "/base-syntax-assignment/index.html"
+  }
+];
